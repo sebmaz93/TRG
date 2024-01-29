5bc43396619bda51aecf93fe11ed1b2b7eb7d00b
sebmaz <seb.maz93@icloud.com>
2024-01-29 12:28:03 +0400

    WIP: go get config

--- go/pkg/config/config.go
+++ go/pkg/config/config.go
@@ -1,5 +1,11 @@
 package cfg
 
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
 type Operation = int
 
 const (
@@ -13,4 +19,64 @@ type Config struct {
 	Operation Operation
 	Pwd       string
 	Config    string
+}
+
+func getPwd(opts Opts) (string, error) {
+	if opts.Pwd != "" {
+		return opts.Pwd, nil
+	}
+
+	return os.Getwd()
+	// we can opt this out since we are returning the same type from the func (string, error)
+	// if err != nil {
+	// 	return "", err
+	// }
+}
+
+func getConfig(opts Opts) (string, error) {
+	if opts.Config != "" {
+		return opts.Config, nil
+	}
+
+	xdg, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+
+	return path.Join(config, "projector", "projector.json"), nil
+}
+
+func getArgs(opts Opts) ([]string, error) {
+	if len(opts.Args) == 0 {
+		return []string{}, nil
+	}
+
+	if opts.Args[0] == "add" {
+		if len(opts.Args) != 3 {
+			return nil, fmt.Errorf("add reqires 2 args, but received %v", len(opts.args))
+		}
+		return opts.Args[1:], nil
+	}
+
+	if opts.Args[0] == "rm" {
+	}
+}
+
+func getOperation(opts Opts) Operation {
+	if len(opts.Args) == 0 {
+		return Print
+	}
+
+	if opts.Args[0] == "add" {
+		return Add
+	}
+
+	if opts.Args[0] == "rm" {
+		return Remove
+	}
+
+	return Print
+}
+
+func NewConfig() (*Config, error) {
 }
